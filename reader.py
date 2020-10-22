31cade9618d405c15e678c560585a545dd1fa6d8
JayateerthDambal <jayateerth.dambal@gmail.com>
2020-10-22 12:15:22 +0530

    Adding Correct Files

--- reader.py
+++
@@ -1,9 +0,0 @@
-import os
-from configparser import ConfigParser
-path = 'cinfig.ini'
-parser = ConfigParser()
-parser.read(path)
-
-print(parser.sections())
-print(parser['AUTH']['username'])
-print(parser['AUTH']['password'])
